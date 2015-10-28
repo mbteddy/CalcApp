59627f3ab35ccdde7905c032a2daa1d94d69d077
Teddy Pappas <mbteddy@verizon.net>
2015-10-27 21:05:48 -0700

    first commit

---
+++ CalcAppTests/CalcAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalcAppTests.swift
+//  CalcAppTests
+//
+//  Created by Teddy Pappas on 10/21/15.
+//  Copyright © 2015 Teddy Pappas. All rights reserved.
+//
+
+import XCTest
+@testable import CalcApp
+
+class CalcAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
